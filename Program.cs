9cd1d72a33700967c593e0a732931ed4cdfa6032
Kerry Thomas <thomke@gmail.com>
2013-09-08 17:09:39 -0500

    Lets get this started.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,24 @@ namespace TextAdventure
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
+			//Just Comment the Logo out if you don't want it running. It's just for show right now.
+			Logo ();
+		}
+
+		public static void Logo() {
+
+			Console.ForegroundColor = ConsoleColor.Blue;
+			Console.WriteLine ("   ___,     ______              ___,                                             ");
+			Console.WriteLine ("  /   |    (_) |               /   |     |                                       ");
+			Console.WriteLine (" |    |        | _      _|_   |    |   __|        _   _  _  _|_         ,_    _  ");
+			Console.WriteLine (" |    |      _ ||/  /\\/  |    |    |  /  |  |  |_|/  / |/ |  |  |   |  /  |  |/  ");
+			Console.WriteLine ("  \\__/\\_/   (_/ |__/ /\\_/|_/   \\__/\\_/\\_/|_/ \\/  |__/  |  |_/|_/ \\_/|_/   |_/|__/");
+			Console.ForegroundColor = ConsoleColor.Gray;
+
+			System.Threading.Thread.Sleep (500);
+
+			Console.Clear();
+
 		}
 	}
 }
---
+++ Question.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace TextAdventure
+{
+	public class Question
+	{
+		public Question ()
+		{
+		}
+	}
+}
+
