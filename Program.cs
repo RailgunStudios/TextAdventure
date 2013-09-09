ff790e1f8f82b213b6f48a429012db23664e682b
Kerry Thomas <thomke@gmail.com>
2013-09-08 22:05:39 -0500

    * CreateQuestion.cs: Basic question creation class.
    * Program.cs:
    * Question.cs:
    * QuestionList.cs:
    * TextAdventure.csproj:

--- Program.cs
+++ Program.cs
@@ -1,16 +1,35 @@
 using System;
 
-namespace TextAdventure
-{
-	class MainClass
-	{
-		public static void Main (string[] args)
-		{
+namespace TextAdventure {
+
+	class MainClass {
+
+		public static void Main (string[] args) {
 			//Just Comment the Logo out if you don't want it running. It's just for show right now.
 			Logo ();
+
+			ConsoleKeyInfo info;
+			CreateQuestion test = new CreateQuestion ("Which Direction do you travel?", new string[] {
+				"North",
+				"South",
+				"East",
+				"West"
+			});
+
+			test.addAnswer ("Stay put.");
+
+			test.printQuestion ();
+			info = Console.ReadKey ();
+
+			if (info.KeyChar == 'a') {
+
+				Console.WriteLine ("Why Hello, World");
+
+			}
+
 		}
 
-		public static void Logo() {
+		public static void Logo () {
 
 			Console.ForegroundColor = ConsoleColor.Blue;
 			Console.WriteLine ("   ___,     ______              ___,                                             ");
@@ -22,7 +41,6 @@ namespace TextAdventure
 
 			System.Threading.Thread.Sleep (500);
 
-			Console.Clear();
 
 		}
 	}
